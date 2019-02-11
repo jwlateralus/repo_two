a50513c0659ce618d2751b977f9e630d27fd5476
jwlateralus <47405559+jwlateralus@users.noreply.github.com>
2019-02-11 23:21:23 +0000

    additional features and code cleanup

--- source/header.h
+++ source/header.h
@@ -1,83 +1,81 @@
 
-
-// test-gitHub
-// This module contains user-configurable and other parameters.
-
-//################ User-configurable Parameters ################
-//
-#define NEIGHBORHOOD_MATRIX
-
-// ---- Integer defines ----
-
-// Square root of the number of boids simulated
-#define		SQRT_NUM_BOIDS							(200.0)
-#define		WINDOW_WIDTH							(3 * 1920 / 4)
-#define		WINDOW_HEIGHT							(3 * 1080 / 4)
-
-#define		CUDA_DEVICE								(0)
+//contains user configurable parameters
 
 
-// 1 would be sufficient for a carefully tuned simulation.
-#define		NUM_SIFTS_PER_FRAME						(1)
-
-// number of frames into the past to track with a moving average
-// for determination of physics delta-t's
-#define		NUM_FRAMES_TO_AVERAGE					(6)
-
-
-// how often to print current FPS to console, or comment out altogether to disable
-#define		US_TO_PRINT_FPS							(1000000)
-
-//in neighborhood matrix mode, radius of nearest neighbors
-// to check and perform rules on (if actually within neighbor distance)
-// 6 should be sufficinet  for this
-#define		MOORE_NEIGHBORHOOD_RADIUS				(6)
-
-// CUDA threads per CUDA execution block, in multiples of 32
-#define		THREADS_PER_BLOCK						(128)
-
-// ---- Float defines ----
-
-// length of lines representing boids' position and velocity
-#define		LINE_LENGTH								(5.0f)
-
-// initial velocitys
-#define		fMAX_START_VEL							(40.0f)
-
-// speed limit of boids
-#define		V_LIM									(200.0f)
-
-// speed of physics
-#define		TICK_FACTOR								(0.000005f)
-
-// strength of alignment rule
-#define		ALIGNMENT_STRENGTH_FACTOR				(0.015f)
-
-// strength of repulsion rule 
-#define		REPULSION_STRENGTH_FACTOR				(55.0f)
-#define		EDGE_REPULSION_STRENGTH_FACTOR			(0.0002f)
-#define		CENTER_OF_MASS_STRENGTH_FACTOR			(0.6f)
-//##############################################################
+//#define		enable_FullScreen			//	 Enable/ disable full screen   -- to do
+//#define		screen_Wrapping				//		 Enable / disable screen wrapping 
+#define		root_num_boids						(300.0)   // root of the number of boids
+#define		win_width							(3 * 1920 / 4) // width of the window
+#define		win_height							(3 * 1080 / 4) // height of the window
+#define		CUDA_device								(0)  // Cuda device init
 
 
-#define	NUM_BOIDS					4000
-#define US_PER_SECOND				(1000000)
-#define THREE_OVER_PI				(0.95492965855137f);
+											// number_frames_to_average	 number of frames into the past to track with 
+#define		number_frames_to_average					(1)
+											//a moving average for determination of physics delta-t's
+
+#define		frame_rate_print_freq (10000000)
+											//required to get fps info on console, how often to print
+
+											// CUDA threads per CUDA execution block. 
+#define		THREADS_PER_BLOCK (128)
+											//Empirically set to 128, multiples of 32 
+
+#define		boid_length	(1.2f)
+
+
+#define		velocity_limit (200.0f)  // speed limit of boids
+
+											// speed of the boids and boids within the flock
+#define		flock_phys_speed_  (0.000006f)
+											// higher number means higherspped movement when flying around 
+
+
+/*
+		Rule 1:Boids try to match velocity with near boids.
+		Rule 2: Boids try to keep a small distance away from other objects (including other boids)
+		Rule 3: Boids try to fly towards the centre of mass of neighbouring boids.
+*/
+
+#define		boids_Alignment_Rule_value				(0.015f)	// magnitude of the alignment rule 
+
+
+#define		boids_Seperation_Rule_value				(55.0f)   // magnitude of the repel rule, prevents boids from running into each other
+
+
+#define		boids_Cohesion_Rule_value				(0.6f)		// magnitude of the centre of mass or the cohession rule 
+
+
+#define neighbourhood_distance			(P_MAX / 13)               //// max distance to be considered a neighbor
+#define neighbourhood_distance_sqrd		(neighbourhood_distance * neighbourhood_distance)
+
+
+
+#define		screen_edge_avoidance			(0.0002f)			// magnitude of the  edge repulsion rule, bounce off the screen when particles hit the edge of the screen
+
+
+
+
+
+#define		mouse_button_press_behaviour			(100.0f)  // strength of one mouse button down mouse attaction/repulsion, flip the sign to switch between attraction and repulsion
+#define		both_mouse_buttons_pressed_behaviour	(6000.0f) // strength of one mouse button down mouse attaction/repulsion, flip the sign to switch between attraction and repulsion
+
+
+
+#define	NUM_BOIDS					5000  //root_num_boids*root_num_boids
+
+
+#define simulation_update_per_seconds		(10000000)
+#define THREE_OVER_PI				(1.464591888f);
 #define fPI							(3.14159265359f);
 
-#define NUM_BLOCKS	256
+#define NUM_BLOCKS	521 //				((NUM_BOIDS - 1) / THREADS_PER_BLOCK + 1)
 
-// square coordinate system max value (for screen independence)
+// in game screen coordinate, maximum value for better visuals
 #define P_MAX						(10000)
 #define fP_MAX						(10000.0f)
 #define fHALF_P_MAX					(5000.0f)
-#define BOID_STARTING_BOX			(P_MAX / SQRT_NUM_BOIDS) 
-// max distance to be considered a neighbor
-#define NEIGHBOR_DISTANCE			(P_MAX / 13)
-#define NEIGHBOR_DISTANCE_SQUARED	(NEIGHBOR_DISTANCE * NEIGHBOR_DISTANCE)
 #define NUM_HSV_SECTORS				(6)
 #define MAX_RGB						(255)
 #define fMAX_RGB					(255.0f)
-#define	V_LIM_2						(V_LIM * V_LIM)
 #define PREVENT_ZERO_RETURN			(0.000000001f)
-
